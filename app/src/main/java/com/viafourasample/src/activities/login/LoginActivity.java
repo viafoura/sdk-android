f57065555590bc74f9c106e6e282b76bfa326b8d
Martin De Simone <martindesimone481@gmail.com>
2023-01-04 00:18:17 -0300

    Remove social login call

--- app/src/main/java/com/viafourasample/src/activities/login/LoginActivity.java
+++ app/src/main/java/com/viafourasample/src/activities/login/LoginActivity.java
@@ -36,19 +36,6 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Log-in");
 
-
-        viewModel.socialLogin("639c9626-972f-4b7e-96bf-bc8f0b1c020b", new LoginViewModel.SocialLoginCallback() {
-            @Override
-            public void onSuccess() {
-
-            }
-
-            @Override
-            public void onError() {
-
-            }
-        });
-
         ((ProgressBar) findViewById(R.id.login_loading)).getIndeterminateDrawable().setColorFilter(
                 VFDefaultColors.getInstance().colorPrimaryDefault,
                 android.graphics.PorterDuff.Mode.SRC_IN);
