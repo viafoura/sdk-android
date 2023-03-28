8d347f2941c9aa6599d8c3bd41f53b1e40c77e84
Martin De Simone <martindesimone481@gmail.com>
2023-03-28 17:02:34 -0400

    Notification bell

--- app/src/main/java/com/viafourasample/src/activities/main/MainActivity.java
+++ app/src/main/java/com/viafourasample/src/activities/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.viafourasample.src.activities.main;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -15,16 +16,28 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.facebook.Profile;
+import com.facebook.login.Login;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.viafoura.sampleapp.R;
 import com.viafourasample.src.activities.login.LoginActivity;
+import com.viafourasample.src.activities.profile.ProfileActivity;
 import com.viafourasample.src.activities.settings.SettingsActivity;
 import com.viafourasample.src.fragments.home.HomeFragment;
 import com.viafourasample.src.fragments.livechat.LiveChatFragment;
 import com.viafourasample.src.managers.ColorManager;
+import com.viafourasample.src.model.IntentKeys;
+import com.viafourasdk.src.interfaces.NotificationBellClickedInterface;
+import com.viafourasdk.src.interfaces.VFLoginInterface;
+import com.viafourasdk.src.model.local.VFColors;
+import com.viafourasdk.src.model.local.VFSettings;
+import com.viafourasdk.src.model.local.VFTheme;
 import com.viafourasdk.src.services.auth.AuthService;
+import com.viafourasdk.src.view.notificationBell.VFNotificationBellView;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private MainViewModel viewModel = new MainViewModel();
@@ -55,10 +68,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        setupNotificationBell();
+
         final Drawable upArrow = getResources().getDrawable(R.drawable.icon_settings);
         upArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupNotificationBell(){
+        VFColors colors = new VFColors(ContextCompat.getColor(getApplicationContext(), R.color.colorVfDark), ContextCompat.getColor(getApplicationContext(), R.color.colorVf), Color.WHITE);
+        VFSettings settings = new VFSettings(colors);
+        VFNotificationBellView bellView = findViewById(R.id.home_bell);
+        bellView.applySettings(settings);
+        bellView.setTheme(VFTheme.dark);
+        bellView.setBellClickedInterface(new NotificationBellClickedInterface() {
+            @Override
+            public void bellPressed(UUID userUUID) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                intent.putExtra(IntentKeys.INTENT_USER_UUID, userUUID.toString());
+                intent.putExtra(IntentKeys.INTENT_USER_PRESENTATION_TYPE, "feed");
+                startActivity(intent);
+            }
+        });
+        bellView.setLoginInterface(new VFLoginInterface() {
+            @Override
+            public void startLogin() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
 
     private void setCurrentFragment(boolean isHome){
