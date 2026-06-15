f8fa133987e83da37822f0e8678eac1724a9a512
martindesimone <martindesimone481@gmail.com>
2026-06-15 11:48:01 -0300

    Test site updates

--- app/src/main/java/com/viafourasample/src/activities/livequestions/LiveQuestionsActivity.java
+++ app/src/main/java/com/viafourasample/src/activities/livequestions/LiveQuestionsActivity.java
@@ -1,9 +1,13 @@
 package com.viafourasample.src.activities.livequestions;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
@@ -20,19 +24,27 @@ import com.viafourasdk.src.model.local.VFActionType;
 import com.viafourasdk.src.model.local.VFArticleMetadata;
 import com.viafourasdk.src.model.local.VFColors;
 import com.viafourasdk.src.model.local.VFSettings;
-import android.content.Intent;
 
 public class LiveQuestionsActivity extends AppCompatActivity implements VFActionsInterface {
+
+    private String containerId;
+    private String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_questions);
 
-        String title = getIntent().getStringExtra(IntentKeys.INTENT_STORY_TITLE);
-        String containerId = getIntent().getStringExtra(IntentKeys.INTENT_CONTAINER_ID);
+        title = getIntent().getStringExtra(IntentKeys.INTENT_STORY_TITLE);
+        containerId = getIntent().getStringExtra(IntentKeys.INTENT_CONTAINER_ID);
         if (title != null) setTitle(title);
 
+        loadFragment(containerId);
+
+        if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void loadFragment(String id) {
         VFColors colors = new VFColors(
             ContextCompat.getColor(getApplicationContext(), R.color.colorVfDark),
             ContextCompat.getColor(getApplicationContext(), R.color.colorVf)
@@ -46,7 +58,7 @@ public class LiveQuestionsActivity extends AppCompatActivity implements VFAction
         );
 
         VFLiveQuestionsFragment fragment = VFLiveQuestionsFragment.newInstance(
-            containerId != null ? containerId : "test-livequestions",
+            id != null ? id : "test-livequestions",
             metadata,
             vfSettings,
             20,
@@ -58,14 +70,42 @@ public class LiveQuestionsActivity extends AppCompatActivity implements VFAction
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.live_questions_container, fragment);
         ft.commit();
+    }
 
-        if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    private void showChangeIdDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Live Q&A container ID");
+        builder.setMessage("Enter a container ID for Live Q&A");
+
+        final EditText input = new EditText(this);
+        input.setText(containerId);
+        builder.setView(input);
+
+        builder.setPositiveButton("Accept", (dialog, which) -> {
+            String newId = input.getText().toString().trim();
+            if (!newId.isEmpty()) {
+                containerId = newId;
+                loadFragment(containerId);
+            }
+        });
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+
+        builder.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_live_questions, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             onBackPressed();
+            return true;
+        } else if (item.getItemId() == R.id.action_change_id) {
+            showChangeIdDialog();
             return true;
         }
         return super.onOptionsItemSelected(item);
